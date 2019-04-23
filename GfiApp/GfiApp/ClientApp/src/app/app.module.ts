e21b3a2163018aa55b8415492b410cfbf6ed0689
lukassz111 <lukassz111@gmail.com>
2019-04-23 11:20:53 +0200

    usunięte nieporzebne rzeczy i dodano translacje

--- GfiApp/GfiApp/ClientApp/src/app/app.module.ts
+++ GfiApp/GfiApp/ClientApp/src/app/app.module.ts
@@ -1,28 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { UsersTableComponent } from './components/users-table/users-table.component';
 import { UsersTableRowComponent } from './components/users-table-row/users-table-row.component';
 import { UserService } from './service/user.service';
 import { UserEditFormComponent } from './components/user-edit-form/user-edit-form.component';
 import { FormValidInfoComponent } from './components/form-valid-info/form-valid-info.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http,'./assets/i18n/','.json');
+}
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     UsersTableComponent,
     UsersTableRowComponent,
     UserEditFormComponent,
@@ -32,13 +30,15 @@ import { FormValidInfoComponent } from './components/form-valid-info/form-valid-
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ]),
 
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
 
   ],
   providers: [UserService],
